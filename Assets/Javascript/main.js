2d713b43428e6bf2104ce9c7d8f3f9f373411e8e
Abhishek Chaudhary <abhishekchaudhary1442016@gmail.com>
2022-04-19 14:22:29 +0930

    added moment date and time

--- Assets/Javascript/main.js
+++ Assets/Javascript/main.js
@@ -1,3 +1,6 @@
 var key = '64f2ee2a8261daa4d9f780f5b365f275';
 var city = "Sydney"
 
+//Grabs the current date and time
+var date = moment().format('dddd, MMMM Do YYYY');
+var dateTime = moment().format('YYYY-MM-DD HH:MM:SS')
