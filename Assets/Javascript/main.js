c542964d2b8eea9e6d7393919e7c56e9becd72fd
Abhishek Chaudhary <abhishekchaudhary1442016@gmail.com>
2022-04-19 14:31:49 +0930

    added functions for search history

--- Assets/Javascript/main.js
+++ Assets/Javascript/main.js
@@ -4,3 +4,48 @@ var city = "Sydney"
 //Grabs the current date and time
 var date = moment().format('dddd, MMMM Do YYYY');
 var dateTime = moment().format('YYYY-MM-DD HH:MM:SS')
+
+var cityHistory = [];
+//Function saves city into history
+$('.search').on("click", function (event) {
+	event.preventDefault();
+	city = $(this).parent('.btnPar').siblings('.textVal').val().trim();
+	if (city === "") {
+		return;
+	};
+	cityHistory.push(city);
+
+	localStorage.setItem('city', JSON.stringify(cityHistory));
+	fiveForecastEl.empty();
+	getHistory();
+	getWeatherToday();
+});
+
+
+//Will create buttons based on search history 
+var contHistEl = $('.cityHistory');
+function getHistory() {
+	contHistEl.empty();
+
+	for (let i = 0; i < cityHistory.length; i++) {
+
+		var rowEl = $('<row>');
+		var btnEl = $('<button>').text(`${cityHistory[i]}`)
+
+		rowEl.addClass('row histBtnRow');
+		btnEl.addClass('btn btn-outline-secondary histBtn');
+		btnEl.attr('type', 'button');
+
+		contHistEl.prepend(rowEl);
+		rowEl.append(btnEl);
+	} if (!city) {
+		return;
+	}
+	//Allows the buttons to start a search as well
+	$('.histBtn').on("click", function (event) {
+		event.preventDefault();
+		city = $(this).text();
+		fiveForecastEl.empty();
+		getWeatherToday();
+	});
+};
